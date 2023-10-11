c65c30e3b60a8c57130a60578f5843fc9e71e3dd
Gianluca GL <grancicg@gmail.com>
2023-10-10 21:00:04 -0300

    FIX: reemplazando BorderPane por gridpane

--- src/main/java/com/mspdevs/mspfxmaven/controllers/VentanaPrincipalController.java
+++ src/main/java/com/mspdevs/mspfxmaven/controllers/VentanaPrincipalController.java
@@ -102,7 +102,7 @@ public class VentanaPrincipalController implements Initializable {
     
     @FXML
     void abrirVentanaCompras(ActionEvent event) throws IOException {
-    	BorderPane centro = FXMLLoader.load(getClass().getResource("/com/mspdevs/mspfxmaven/views/VentanaCompras.fxml"));
+    	GridPane centro = FXMLLoader.load(getClass().getResource("/com/mspdevs/mspfxmaven/views/VentanaCompras.fxml"));
     	bpane.setCenter(centro);
     }
     
